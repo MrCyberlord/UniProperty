ac29a7e78f6ce286e8cb8a2764c32b708bf02a2c
MrCyberlord <amanjain2691@gmail.com>
2025-01-16 16:07:46 +0530

    changed the title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Property Listings',
+  title: 'Uni Properties',
   description: 'Find your dream property',
 }
 
